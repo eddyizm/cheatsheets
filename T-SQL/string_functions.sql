aef01db1a97f014d605b66e61586f4481fc5dae9
eddyizm <eddyizm@gmail.com>
2022-07-17 10:48:40 -0700

    changed sql folder to t-sql since its all MS SQL SERVER. Added string functions

---
+++ T-SQL/string_functions.sql
@@ -0,0 +1,30 @@
+/* string manipulation functions */
+
+-- convert number to letter in alphabet
+
+DECLARE @number INT = 5; 
+SELECT  Letter = CHAR(64 + @number);
+
+--string split
+SELECT ProductId, Name, value  
+FROM Product  
+    CROSS APPLY STRING_SPLIT(Tags, ',');  
+
+-- convert scientific notation number to int
+select str(cast('7.5013e+006' as real) )
+
+--format only 
+select format(convert(NUMERIC(18, 2), ISNULL(NULLIF(<VARCHAR>, ''), '0')) / 100, '0.00')
+
+-- extract file name from folder path
+select SUBSTRING(FileName, LEN(FileName) + 2 - CHARINDEX('\',REVERSE(FileName)), LEN(FileName))')
+
+-- Example of  Concat with single quotes for another IN query
+SELECT  ('''' + RTRIM(ITEMNMBR)+''''+ ',')
+
+-- convert varchar to decimal 
+select convert(NUMERIC(18, 2), ISNULL(NULLIF(Interest, ''), '0'))
+
+-- extract folder path from full file path
+select REVERSE( SUBSTRING(REVERSE(Location), CHARINDEX('\', REVERSE(Location)) , LEN (Location))) Folder))
+
