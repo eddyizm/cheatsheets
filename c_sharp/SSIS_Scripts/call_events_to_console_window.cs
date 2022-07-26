04121d03ff99de0581a21764fbc66e7c80ba9dbb
eddyizm <eddyizm@gmail.com>
2022-07-25 21:41:19 -0700

    ssis c# scripts. call to window to debug

---
+++ c_sharp/SSIS_Scripts/call_events_to_console_window.cs
@@ -0,0 +1,5 @@
+/* print details to console log. */
+Dts.Events.FireInformation(0, "", "-----------------------------------------------", "", 0, ref no);
+Dts.Events.FireInformation(0, "", files.Length.ToString(), "", 0, ref no);
+Dts.Events.FireInformation(0, "", "-----------------I AM HERE ------------------", "", 0, ref no);
+Dts.Events.FireInformation(0, "", "-----------------------------------------------", "", 0, ref no);
