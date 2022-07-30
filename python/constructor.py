87d816836f2899490a75a2d3452f36cd0203e13b
eddyizm <eddyizm@gmail.com>
2022-07-29 21:10:44 -0700

    OOP constructor sample for python

---
+++ python/constructor.py
@@ -0,0 +1,31 @@
+# class object sample with initialized properties. 
+
+class Animal:
+    def __init__(self, type, name, sound):
+        self._type = type
+        self._name = name
+        self._sound = sound
+
+    def type(self):
+        return self._type
+
+    def name(self):
+        return self._name
+
+    def sound(self):
+        return self._sound
+
+def print_animal(o):
+    if not isinstance(o, Animal):
+        raise TypeError('print_animal(): requires an Animal')
+    print('The {} is named "{}" and says "{}".'.format(o.type(), o.name(), o.sound()))
+
+
+def main():
+    a0 = Animal('kitten', 'fluffy', 'rwar')
+    a1 = Animal('duck', 'donald', 'quack')
+    print_animal(a0)
+    print_animal(a1)
+    print_animal(Animal('velociraptor', 'veronica', 'hello'))
+
+if __name__ == '__main__': main()
