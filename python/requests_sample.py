bbe5bc8a1b50a6618c5a70b8d3fe9822fb48b50d
eddyizm <eddyizm@gmail.com>
2022-08-16 16:18:27 -0700

    request py

---
+++ python/requests_sample.py
@@ -0,0 +1,11 @@
+# download files 
+import requests
+import os
+BASE_URL = 'https://media.grc.com/sn/sn-'	
+dir = r'/home/<USER>/Music'
+
+
+for x in range(21,100):
+	file = '0'+str(x)+'.mp3'
+	r = requests.get(BASE_URL+file, allow_redirects=True)
+	open(os.path.join(dir, file), 'wb').write(r.content)
