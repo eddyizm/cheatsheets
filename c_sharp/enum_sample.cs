3aabf8f1954bb68725259229edcd5747403c18ee
eddyizm <eddyizm@gmail.com>
2022-10-18 17:02:17 -0700

    added enum sample

---
+++ c_sharp/enum_sample.cs
@@ -0,0 +1,15 @@
+// enums
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            enum Day {Sat, Sun, Mon, Tue, Wed, Thu, Fri};  
+        }
+        
+        
+    }
+}
