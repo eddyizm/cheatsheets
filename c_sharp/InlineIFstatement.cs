8cc267d92c06f2167b7b189fe46f24c48ee6ea82
eddyizm <eddyizm@gmail.com>
2022-10-31 19:21:16 -0700

    inline if snippet c#

---
+++ c_sharp/InlineIFstatement.cs
@@ -0,0 +1,12 @@
+// Inline IF statement 
+
+condition ? first_expression : second_expression;
+
+/*boolean*/ condition ? /*if true this one is triggered */ first_expression : /*else the second one*/ second_expression;
+
+
+// msdn example setting a variable
+classify = (input > 0) ? "positive" : "negative";
+
+// checking if value equals 1, return first value , else return second value
+return (value == 1 ? Periods.VariablePeriods : Periods.FixedPeriods);
