45685862753d51317417e4382863bcbbf8565747
eddyizm <eddyizm@gmail.com>
2022-11-18 15:27:06 -0800

    find item in array js

--- javascript/js_cheatsheet.js
+++ javascript/js_cheatsheet.js
@@ -1,4 +1,16 @@
 // Javascript Cheatsheet. 
 
 // in <a> tag works
-onclick="window.close()"
+onclick="window.close()"    
+
+// Find an object in an array by one of its properties
+const inventory = [
+    { name: "apples", quantity: 2 },
+    { name: "bananas", quantity: 0 },
+    { name: "cherries", quantity: 5 },
+  ];
+  
+  const result = inventory.find(({ name }) => name === "cherries");
+  
+  console.log(result); // { name: 'cherries', quantity: 5 }
+  
