b0453ac450750012429f1c5b333868b9ca560a25
eddyizm <eddyizm@gmail.com>
2023-03-22 08:28:06 -0700

    check if value exists in column - field

--- python/pandas_cheatsheet.py
+++ python/pandas_cheatsheet.py
@@ -1,3 +1,17 @@
+# check if a value exists, returns bool  
+>>> df = pd.DataFrame(np.arange(10).reshape(-1, 2), columns=['A', 'B'])
+>>> df
+   A  B
+0  0  1
+1  2  3
+2  4  5
+3  6  7
+4  8  9
+>>> 3 in df.B.values
+True
+>>> 4 in df.B.values
+False
+
 # swap column values on condition
  df['first_name'],df['last_name']=np.where(df['country']=='Japan',(df['last_name'],df['first_name']),(df['first_name'],df['last_name']))
 
