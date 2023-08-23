04634c8c8869ec7a72c384a5ce2bb06a5b1c3aab
eddyizm <eddyizm@gmail.com>
2023-08-23 12:42:54 -0700

    show all foreign keys in postgres db

--- postgres/metadata.sql
+++ postgres/metadata.sql
@@ -1,3 +1,12 @@
+-- show all foreign keys
+SELECT conrelid::regclass AS table_name, 
+       conname AS foreign_key, 
+       pg_get_constraintdef(oid) 
+FROM   pg_constraint 
+WHERE  contype = 'f' 
+AND    connamespace = 'public'::regnamespace   
+ORDER  BY conrelid::regclass::text, contype DESC;
+
 -- list indexes
 select *
 from pg_indexes
