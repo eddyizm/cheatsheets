c0aec077e26ea2ada9e2c68ce547da752b14b4e9
eddyizm <eddyizm@gmail.com>
2023-08-27 09:52:58 -0700

    started new macos scripts

---
+++ linux-bash/macOS.sh
@@ -0,0 +1,10 @@
+### mac os specific commands
+
+# list updates available
+softwareupdate -l
+
+# install 
+softwareupdate -i "Security Update 2020-005-10.13.6"
+
+# install all 
+softwareupdate -i -a
