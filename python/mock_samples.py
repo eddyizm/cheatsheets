4699a604aac727f01feba9900da70c3c140f1a99
eddyizm <eddyizm@gmail.com>
2023-09-25 14:20:45 -0700

    Update mock_samples.py

--- python/mock_samples.py
+++ python/mock_samples.py
@@ -1,3 +1,9 @@
+# set property
+mock = MagicMock()
+mock.configure_mock(name='my_name')
+mock.name
+# returns 'my_name'
+
 # mocking with MagicMock to avoid error: TypeError: 'Mock' object is not subscriptable
 from unittest.mock import MagicMock
 
@@ -10,3 +16,10 @@ mock.__getitem__.return_value = 'new'
 
 print(mock[0])  # 👉️ new
 print(mock[:2])  # 👉️ new
+
+# set return value
+mock = Mock(return_value=3)
+mock.return_value
+# returns 3
+mock()
+# returns 3
