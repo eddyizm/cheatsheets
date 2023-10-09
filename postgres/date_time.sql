d471a6ff36da89a26ca5aede322ba3ec4ce12d96
eddyizm <eddyizm@gmail.com>
2023-10-09 08:09:57 -0700

    find dates one month or days back in postgres

--- postgres/date_time.sql
+++ postgres/date_time.sql
@@ -1,3 +1,9 @@
 -- datetime in postgres is timestamp
 -- convert string to timestamp
 SELECT TIMESTAMP '2001-02-16'
+
+-- get date one month back from current date
+select date_trunc('day', TIMESTAMP '2023-09-20' - interval '1 month');
+
+-- subract days from current date
+SELECT CURRENT_DATE - INTERVAL '10 days';
