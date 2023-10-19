65ac7e479cba1324d48a9b5ac7a6eaa7eb71d463
eddyizm <eddyizm@gmail.com>
2023-10-18 21:00:43 -0700

    added bt cli control to mac

--- linux-bash/macOS.sh
+++ linux-bash/macOS.sh
@@ -11,3 +11,15 @@ softwareupdate -i -a
 
 # show os version  
 sw_vers
+
+# install bluetooth cli utility
+brew install blueutil  
+
+# Print bluetooth status
+blueutil
+
+# Switch bluetooth on
+blueutil --power 1 or blueutil -p 1
+
+# Switch bluetooth off
+blueutil --power 0 or blueutil -p 0
