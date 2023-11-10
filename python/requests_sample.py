ac49d4869989a59630764de8d3b6e798c98ac209
eddyizm <eddyizm@gmail.com>
2023-11-09 20:16:58 -0800

    added request text

--- python/requests_sample.py
+++ python/requests_sample.py
@@ -9,3 +9,8 @@ for x in range(21,100):
 	file = '0'+str(x)+'.mp3'
 	r = requests.get(BASE_URL+file, allow_redirects=True)
 	open(os.path.join(dir, file), 'wb').write(r.content)
+
+# return text of page
+r = requests.get('https://google.com')
+r.raise_for_status()
+print(r.text)
