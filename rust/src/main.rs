9f806a7fcbdab16ef0d494a606d3cdca9ee6da8c
eddyizm <eddyizm@gmail.com>
2023-11-27 21:40:10 -0800

    rust added build name exercise

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,9 +3,11 @@ mod break_sample;
 mod main_loops;
 mod error_handling_sample;
 mod hash_map_sample;
+mod build_name;
 
 fn main(){
-    hash_map_sample::hash_sample();
+    build_name::name_exercise();
+    //hash_map_sample::hash_sample();
     //error_handling_sample::er_sample();
     //main_loops::create_cars()
     //break_sample::main();
