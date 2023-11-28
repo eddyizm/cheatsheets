dae1e304c67d6a6c5ba266d6c19c6d88a82099e3
eddyizm <eddyizm@gmail.com>
2023-11-27 21:29:36 -0800

    added loop sample to rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,9 @@
 mod hello_world;
 mod break_sample;
+mod main_loops;
 
 fn main(){
-    break_sample::main();
+    main_loops::create_cars()
+    //break_sample::main();
     //hello_world::hw();
 }
