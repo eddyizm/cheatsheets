d4ee181e2c65ceccbbcc6f1446843de29410f7f6
eddyizm <eddyizm@gmail.com>
2023-12-03 10:08:49 -0800

    add rust lifetime exercise

---
+++ rust/src/lifetimes_sample.rs
@@ -0,0 +1,22 @@
+fn copy_and_return<'a>(vector: &'a mut Vec<String>, value: &'a str) -> &'a str {
+    vector.push(String::from(value));
+    return value
+
+}
+
+pub fn lifetime_main(){
+    let name1 = "Joe";
+    let name2 = "Chris";
+    let name3 = "Anne";
+
+    let mut names = Vec::new();
+
+    assert_eq!("Joe", copy_and_return(&mut names, &name1));
+    assert_eq!("Chris", copy_and_return(&mut names, &name2));
+    assert_eq!("Anne", copy_and_return(&mut names, &name3));
+
+    assert_eq!(
+        names,
+        vec!["Joe".to_string(), "Chris".to_string(), "Anne".to_string()]
+    )
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -9,31 +9,12 @@
 // mod result_sample;
 // mod vector_sample;
 // mod ownership_example;
+mod lifetimes_sample;
 
-
-// TODO: modify only this function.
-fn copy_and_return<'a>(vector: &'a mut Vec<String>, value: &'a str) -> &'a str {
-    vector.push(String::from(value));
-    return value
-
-}
 
 fn main(){
-    let name1 = "Joe";
-    let name2 = "Chris";
-    let name3 = "Anne";
 
-    let mut names = Vec::new();
-
-    assert_eq!("Joe", copy_and_return(&mut names, &name1));
-    assert_eq!("Chris", copy_and_return(&mut names, &name2));
-    assert_eq!("Anne", copy_and_return(&mut names, &name3));
-
-    assert_eq!(
-        names,
-        vec!["Joe".to_string(), "Chris".to_string(), "Anne".to_string()]
-    )
-
+    lifetimes_sample::lifetime_main();
     // ownership_example::annotated_lifetimes_in_types();
     // vector_sample::main_vector();
     // result_sample::main_division_example()
