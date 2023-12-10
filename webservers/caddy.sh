4f5189941a67f9a489980eed36618f760811a3b6
eddyizm <eddyizm@gmail.com>
2023-12-10 15:39:16 -0800

    added caddy configs for fail2ban

--- webservers/caddy.sh
+++ webservers/caddy.sh
@@ -1,2 +1,7 @@
 # holy crap caddy is simple
 caddy reverse-proxy --from <DOMAIN> --to http://<REVERSE_IP>:<PORT>
+
+# set up service to run after installing binary and adding to $PATH
+which caddy # /usr/bin/caddy
+
+sudo groupadd --system caddy
