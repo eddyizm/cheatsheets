23e7997ba04f021f2532a4956a6b32fb542d52b8
eddyizm <eddyizm@gmail.com>
2023-12-14 07:42:50 -0800

    get row count in polars

--- python/polars.py
+++ python/polars.py
@@ -1,2 +1,8 @@
+# get row count
+100 = df.height
+
+# pandas style also works
+100, 10 = df.shape
+
 # update field
 df[3, 'b'] = 99  # where b is the column name, 3 is the index (row 3 in this case) and 99 the new value
