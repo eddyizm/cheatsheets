2c738dcfc7cc45bb27cebd32d17f0beb1ad9f12d
eddyizm <eddyizm@gmail.com>
2024-02-19 20:47:09 -0800

    added gcloud scp copying from compute engine

--- gcloud.sh
+++ gcloud.sh
@@ -5,3 +5,8 @@ gcloud config list
 
 # get specific account values
 gcloud config get-value account
+
+# copying files from vm  
+gcloud compute scp instance-1:/etc/apache2/apache2.conf %userprofile%/desktop/apache2.conf
+gcloud compute scp instance-1:/home/eddyizm/sitefiles/eddyizm/main/main/settings.py %userprofile%/HP/backups/settings.py"
+
