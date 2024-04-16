a4028f1c0bebbb878326f9ae23aa8cbc1ea407e8
eddyizm <eddyizm@gmail.com>
2024-04-15 21:09:14 -0700

    postgres: add pk to existing table

--- postgres/alter.sql
+++ postgres/alter.sql
@@ -1,2 +1,6 @@
 -- add timestamp column
 ALTER TABLE mytable ADD COLUMN created_at TIMESTAMP DEFAULT NOW()
+
+-- add id column with primary key to existing table
+alter table table_name
+add column id bigint primary key generated always as identity;
