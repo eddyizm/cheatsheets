f08db436f0ef4362d049b43739fc4217ae63b75c
eddyizm <eddyizm@gmail.com>
2025-08-26 08:58:50 -0700

    android: cleaning bixby from samsung device

--- Android.sh
+++ Android.sh
@@ -15,6 +15,22 @@ pm list packages | grep 'samsung'
 # remove
 pm uninstall --user 0 <PACKAGE NAME>
 
+# clean up samsung bixby crap
+adb shell pm uninstall -k --user 0 com.samsung.android.bixby.agent
+adb shell pm uninstall -k --user 0 com.samsung.android.bixby.es.globalaction
+adb shell pm uninstall -k --user 0 com.samsung.android.bixbyvision.framework
+adb shell pm uninstall -k --user 0 com.samsung.android.bixby.wakeup
+adb shell pm uninstall -k --user 0 com.samsung.android.bixby.plmsync
+adb shell pm uninstall -k --user 0 com.samsung.android.bixby.voiceinput
+adb shell pm uninstall -k --user 0 com.samsung.systemui.bixby
+adb shell pm uninstall -k --user 0 com.samsung.android.bixby.agent.dummy
+adb shell pm uninstall -k --user 0 com.samsung.android.app.settings.bixby
+adb shell pm uninstall -k --user 0 com.samsung.systemui.bixby2
+adb shell pm uninstall -k --user 0 com.samsung.android.bixby.service
+adb shell pm uninstall -k --user 0 com.samsung.android.app.routines
+adb shell pm uninstall -k --user 0 com.samsung.android.visionintelligence
+adb shell pm uninstall -k --user 0 com.samsung.android.app.spage
+
 # list emulaters
 emulator -list-avds
 
