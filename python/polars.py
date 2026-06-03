358d137bd73d8495b73721966daace9bef7b1c00
eddyizm <eddyizm@gmail.com>
2026-06-02 19:55:10 -0700

    python: polars drop col

--- python/polars.py
+++ python/polars.py
@@ -4,5 +4,8 @@
 # pandas style also works
 100, 10 = df.shape
 
+# drop column
+df = df.drop("col")
+
 # update field
 df[3, 'b'] = 99  # where b is the column name, 3 is the index (row 3 in this case) and 99 the new value
