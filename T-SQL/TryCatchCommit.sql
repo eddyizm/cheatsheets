ba78a36f4dd630b86c9f2d1faddc9b6b8c0bcb2f
eddyizm <eddyizm@gmail.com>
2026-09-17 07:31:53 -0700

    tsql: updated catch block

--- T-SQL/TryCatchCommit.sql
+++ T-SQL/TryCatchCommit.sql
@@ -1,29 +1,20 @@
 /* Try Catch with TRANSACTION CONTROL */
 SET ANSI_NULLS OFF;
-SET NOCOUNT ON; 
+SET NOCOUNT ON;
 SET XACT_ABORT ON;
-SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED; 
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 
-BEGIN TRANSACTION;  
-  
-    BEGIN TRY  
+BEGIN TRANSACTION;
+    BEGIN TRY
         -- Generate a constraint violation error.  
-        DELETE FROM Production.Product  
-        WHERE ProductID = 980;  
-    END TRY  
-    BEGIN CATCH  
-        SELECT   
-            ERROR_NUMBER() AS ErrorNumber  
-            ,ERROR_SEVERITY() AS ErrorSeverity  
-            ,ERROR_STATE() AS ErrorState  
-            ,ERROR_PROCEDURE() AS ErrorProcedure  
-            ,ERROR_LINE() AS ErrorLine  
-            ,ERROR_MESSAGE() AS ErrorMessage;  
-      
-        IF @@TRANCOUNT > 0  
-            ROLLBACK TRANSACTION;  
-    END CATCH;  
-  
-IF @@TRANCOUNT > 0  
-    COMMIT TRANSACTION;  
+        DELETE FROM Production.Product
+        WHERE ProductID = 980;
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRANSACTION;
+        THROW; -- Re-throws the active error automatically
+    END CATCH
+END TRANSACTION
+
 GO
